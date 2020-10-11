ac07eab44121e192061e095ce7baf4fd580066d8
EvilMuffinHa <hkailadka88@gmail.com>
2020-10-10 22:20:51 -0400

    button

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import *
+from random import randint as rint
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-	return "in progress!"
+	version = rint(0, 300000000)
+	return render_template('index.html', version=str(version))
+
 
 if __name__ == "__main__":
 	app.run()
